a09c3bceeaa69548a89beb442e8edd26d9923ed3
Aristide Manyessé <aristidemanyesse@gmail.com>
2025-09-05 13:33:54 +0000

    Refactor code structure and remove redundant changes

--- source/MainApp/urls.py
+++ source/MainApp/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('clients/', views.clients_view, name='clients'),
+    path('client/', views.client_view, name='client'),
     path('credits/', views.credits_view, name='credits'),
+    path('credit/', views.credit_view, name='credit'),
     path('epargne/', views.epargne_view, name='epargne'),
     path('rapports/', views.rapports_view, name='rapports'),
 ]
--- source/MainApp/views.py
+++ source/MainApp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from annoying.decorators import render_to
+from faker import Faker
 
 
 def login_view(request):
@@ -7,14 +9,44 @@ def login_view(request):
 def dashboard_view(request):
     return render(request, 'MainApp/dashboard.html')
 
+@render_to('MainApp/clients.html')
 def clients_view(request):
-    return render(request, 'MainApp/clients.html')
+    faker = Faker("fr_FR")
+    ctx = {
+        "faker": faker,
+    }
+    return ctx
+
+
+
+@render_to('MainApp/client.html')
+def client_view(request):
+    faker = Faker("fr_FR")
+    ctx = {
+        "faker": faker,
+    }
+    return ctx
+
 
 def credits_view(request):
     return render(request, 'MainApp/credits.html')
 
+@render_to('MainApp/credit.html')
+def credit_view(request):
+    faker = Faker("fr_FR")
+    ctx = {
+        "faker": faker,
+    }
+    return ctx
+
+
+@render_to('MainApp/epargne.html')
 def epargne_view(request):
-    return render(request, 'MainApp/epargne.html')
+    faker = Faker("fr_FR")
+    ctx = {
+        "faker": faker,
+    }
+    return ctx
 
 def rapports_view(request):
     return render(request, 'MainApp/rapports.html')
